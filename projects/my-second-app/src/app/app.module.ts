f8de0613ff8adfea0cd4cbda8c877cb56f90e09b
VemulaDurgaMahesh <durgamahesh56@gmail.com>
2023-04-13 21:37:00 +0530

    created library

--- projects/my-second-app/src/app/app.module.ts
+++ projects/my-second-app/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyLibModule } from "../../../my-lib/src/lib/my-lib.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MyLibModule
+    ]
 })
 export class AppModule { }
